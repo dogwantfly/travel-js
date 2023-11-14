900ec2e8790b8e94f560df18e786f5f80a00921e
hsin yu <tina2793778@gmail.com>
2023-11-15 01:22:05 +0800

    feat: 🎸 新增 axios 取得資料

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-let data = [
-  {
-    id: 0,
-    name: '肥宅心碎賞櫻3日',
-    imgUrl:
-      'https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80',
-    area: '高雄',
-    description: '賞櫻花最佳去處。肥宅不得不去的超讚景點！',
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: '貓空纜車雙程票',
-    imgUrl:
-      'https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-    area: '台北',
-    description:
-      '乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感',
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: '台中谷關溫泉會1日',
-    imgUrl:
-      'https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-    area: '台中',
-    description:
-      '全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。',
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
 const areaSelect = document.querySelector('[data-area-select]');
 const areaSearchSelect = document.querySelector('[data-search-select]');
-const areaArr = [...new Set([...data.map((item) => item.area)])];
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+const textarea = document.querySelector('textarea');
+const submitBtn = document.querySelector('button[type=submit]');
+const list = document.querySelector('[data-list]');
+let areaArr;
+let ticketData;
 
-areaSelect.innerHTML = `
+function initSelect(data) {
+  areaArr = [...new Set([...data.map((item) => item.area)])];
+  areaSelect.innerHTML = `
 <option value="" disabled selected>請填寫景點地區</option>
 ${areaArr
   .map(
@@ -48,57 +19,15 @@ ${areaArr
 `
   )
   .join('')}`;
-
-areaSearchSelect.innerHTML = `<option value="" disabled selected>地區搜尋</option><option value="all">全部地區</option>${areaArr
-  .map(
-    (item) => `
+  areaSearchSelect.innerHTML = `<option value="" disabled selected>地區搜尋</option><option value="all">全部地區</option>${areaArr
+    .map(
+      (item) => `
 <option value="${item}">${item}</option>
 `
-  )
-  .join('')}`;
-
-const form = document.querySelector('form');
-const inputs = document.querySelectorAll('input');
-const textarea = document.querySelector('textarea');
-const submitBtn = document.querySelector('button[type=submit]');
-
-form.addEventListener('input', () => {
-  if (
-    ![...inputs].every((item) => item.value) ||
-    !textarea.value ||
-    !areaSelect.value
-  ) {
-    submitBtn.disabled = true;
-  } else {
-    submitBtn.disabled = false;
-  }
-});
-function disableSubmitBtn() {
-  if (![...inputs].every((item) => item.value)) {
-    submitBtn.disabled = true;
-  }
+    )
+    .join('')}`;
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const inputsValue = [...inputs].map((item) => item.value);
-
-  const newData = {
-    id: data.length + 1,
-    name: inputsValue[0],
-    imgUrl: inputsValue[1],
-    area: areaSelect.value,
-    description: textarea.value,
-    group: inputsValue[3],
-    price: inputsValue[2],
-    rate: inputsValue[4],
-  };
-  data.push(newData);
-  renderData(data);
-  form.reset();
-  disableSubmitBtn();
-});
-const list = document.querySelector('[data-list]');
 function renderData(filterData) {
   let str = '';
   if (!filterData.length) {
@@ -123,10 +52,10 @@ function renderData(filterData) {
       <div class="p-5 text-primary-500 flex flex-col bg-white shadow card-content rounded-b">
       <h3 class="title font-medium text-2xl pb-1 border-b-2 border-primary-600 mb-4">
       ${item.name}
-     </h3>
-     <p class="text-secondary-600 mb-5 grow">
-       ${item.description}
-     </p>
+    </h3>
+    <p class="text-secondary-600 mb-5 grow">
+    ${item.description}
+    </p>
         <div class="flex justify-between items-center">
         <p class="font-medium">
         <span><i class="fas fa-exclamation-circle"></i></span>
@@ -144,8 +73,58 @@ function renderData(filterData) {
   }
   list.innerHTML = str;
 }
-renderData(data);
-disableSubmitBtn();
+
+function disableSubmitBtn() {
+  if (![...inputs].every((item) => item.value)) {
+    submitBtn.disabled = true;
+  }
+}
+
+async function getData() {
+  try {
+    const response = await axios.get(
+      'https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json'
+    );
+    ticketData = response.data.data;
+    initSelect(ticketData);
+    renderData(ticketData);
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+form.addEventListener('input', () => {
+  if (
+    ![...inputs].every((item) => item.value) ||
+    !textarea.value ||
+    !areaSelect.value
+  ) {
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const inputsValue = [...inputs].map((item) => item.value);
+
+  const newData = {
+    id: data.length + 1,
+    name: inputsValue[0],
+    imgUrl: inputsValue[1],
+    area: areaSelect.value,
+    description: textarea.value,
+    group: parseInt(inputsValue[3]),
+    price: parseInt(inputsValue[2]),
+    rate: parseInt(inputsValue[4]),
+  };
+  data.push(newData);
+  renderData(data);
+  form.reset();
+  disableSubmitBtn();
+});
+
 areaSearchSelect.addEventListener('change', (e) => {
   const filterData =
     e.target.value === 'all'
@@ -158,3 +137,6 @@ areaSearchSelect.addEventListener('change', (e) => {
   searchDataNum.parentNode.classList.add('block');
   searchDataNum.textContent = filterData.length;
 });
+
+getData();
+disableSubmitBtn();
